95ed07d97d8826702c1541288987fd0031ffb668
daisylynlaygan <daisylynlaygan@gmail.com>
2025-03-30 22:37:05 +0700

    Added create user functionality

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -3,7 +3,36 @@ import { userService } from "../_helpers/service";
 
 export const userRouter = Router();
 
+// DELETE /api/users/:id
+userRouter.delete("/:id", async (req, res, next) => {
+  userService
+    .delete(Number(req.params.id))
+    .then(() => res.json({ message: "User deleted" }))
 
+// PUT /api/users/:id
+userRouter.put("/:id", async (req, res, next) => {
+  userService
+    .update(req.params.id as unknown as number, req.body)
+    .then(() => res.json({ message: "User updated" }))
+
+    .catch(next);
+});
+
+// LISTING  /api/users
+userRouter.get("/", async (req, res, next) => {
+  userService
+    .getAll()
+    .then((users) => res.json(users))
+    .catch(next);
+});
+
+// RETRIEVAL /api/users/:id
+userRouter.get("/:id", async (req, res, next) => {
+  userService
+    .getById(req.params.id as unknown as number)
+    .then((users) => res.json(users))
+    .catch(next);
+});
 
 // POST /api/users
 userRouter.post("/", async (req, res, next) => {
@@ -12,4 +41,3 @@ userRouter.post("/", async (req, res, next) => {
     .then(() => res.json({ message: "User created" }))
     .catch(next);
 });
-

c4215d57165e9765a756e2f92527a9d3b5571162
daisylynlaygan <daisylynlaygan@gmail.com>
2025-03-30 22:19:38 +0700

    Added create user functionality

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -4,17 +4,12 @@ import { userService } from "../_helpers/service";
 export const userRouter = Router();
 
 
-// DELETE /api/users/:id
-userRouter.delete("/:id", async (req, res, next) => {
-  userService
-    .delete(Number(req.params.id))
-    .then(() => res.json({ message: "User deleted" }))
 
-// PUT /api/users/:id
-userRouter.put("/:id", async (req, res, next) => {
+// POST /api/users
+userRouter.post("/", async (req, res, next) => {
   userService
-    .update(req.params.id as unknown as number, req.body)
-    .then(() => res.json({ message: "User updated" }))
-
+    .create(req.body)
+    .then(() => res.json({ message: "User created" }))
     .catch(next);
 });
+
